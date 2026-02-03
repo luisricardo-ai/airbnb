774ac74e47940b612ed58578f8a0d0a1a1797c97
LuisRicardo <palharini.luis@gmail.com>
2026-02-03 19:21:32 -0300

    feat(mart): Add new model to see correlation of reviews on fullmoons

---
+++ airbnb/models/mart/mart_fullmoon_reviews.sql
@@ -0,0 +1,33 @@
+{{ 
+    config(
+        materialized = 'table',
+    ) 
+}}
+
+WITH fct_reviews AS (
+    SELECT 
+        * 
+    FROM 
+        {{ ref('fct_reviews') }}
+),
+full_moon_dates AS (
+    SELECT 
+        * 
+    FROM 
+        {{ ref('seed_full_moon_dates') }}
+)
+
+SELECT
+    fct_reviews.listing_id,
+    fct_reviews.review_date,
+    fct_reviews.reviewer_name,
+    fct_reviews.review_text,
+    fct_reviews.review_sentiment,
+    CASE
+        WHEN full_moon_dates.full_moon_date IS NULL THEN 'not full moon'
+        ELSE 'full moon'
+    END AS is_full_moon
+FROM
+    fct_reviews
+LEFT JOIN full_moon_dates
+    ON (TO_DATE(fct_reviews.review_date) = DATEADD(DAY, 1, full_moon_dates.full_moon_date))
